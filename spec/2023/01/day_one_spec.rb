b4ecbc7f0d86a62bf9b38938d84f5e610c265c93
Tekin Suleyman <tekin@tekin.co.uk>
2023-12-04 11:05:05 +0000

    Failing spec for day one; part 2

--- spec/2023/01/day_one_spec.rb
+++ spec/2023/01/day_one_spec.rb
@@ -1,10 +1,17 @@
 require "2023/01/day_one"
 
 RSpec.describe "DayOne" do
-  context "the example input" do
+  context "the first example input/output" do
     let(:input) { File.read('spec/fixtures/2023/01/example-1.txt') }
     subject(:output) { DayOne.solve(input) }
 
     it { is_expected.to eql 142 }
+  end
+
+  context "the second example input/output" do
+    let(:input) { File.read('spec/fixtures/2023/01/example-2.txt') }
+    subject(:output) { DayOne.solve(input) }
+
+    it { is_expected.to eql 281 }
   end
 end
