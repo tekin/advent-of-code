91d8add07012940afb89e56166094bfeee6451c4
Tekin Suleyman <tekin@tekin.co.uk>
2023-12-04 11:33:18 +0000

    Implement day one; part two

--- 2023/01/day_one.rb
+++ 2023/01/day_one.rb
@@ -1,8 +1,10 @@
+require_relative "number_extractor"
+
 class DayOne
   def self.solve(input)
     input
       .split("\n")
-      .map { |line| line.scan(/\d/) }
+      .map { |line| NumberExtractor.new(line).as_numbers }
       .sum { |numbers| [numbers.first, numbers.last].join.to_i }
   end
 end
