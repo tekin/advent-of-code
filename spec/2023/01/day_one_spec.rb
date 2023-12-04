702caf748d5db894b3d275fa35444f71a129a5f0
Tekin Suleyman <tekin@tekin.co.uk>
2023-12-04 10:55:37 +0000

    Prep for part 2...

--- spec/2023/01/day_one_spec.rb
+++ spec/2023/01/day_one_spec.rb
@@ -2,7 +2,7 @@ require "2023/01/day_one"
 
 RSpec.describe "DayOne" do
   context "the example input" do
-    let(:input) { File.read('spec/fixtures/2023/01/example.txt') }
+    let(:input) { File.read('spec/fixtures/2023/01/example-1.txt') }
     subject(:output) { DayOne.solve(input) }
 
     it { is_expected.to eql 142 }
