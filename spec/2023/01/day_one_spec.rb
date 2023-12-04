4e0abc07198dbabea4c19a3e4839997975345c1e
Tekin Suleyman <tekin@tekin.co.uk>
2023-12-04 10:17:06 +0000

    Failing spec with the sample input

---
+++ spec/2023/01/day_one_spec.rb
@@ -0,0 +1,10 @@
+require "2023/01/day_one"
+
+RSpec.describe "DayOne" do
+  context "the example input" do
+    let(:input) { File.read('spec/fixtures/2023/01/example.txt') }
+    subject(:output) { DayOne.solve(input) }
+
+    it { is_expected.to eql 142 }
+  end
+end
