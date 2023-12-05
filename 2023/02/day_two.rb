66ad3cfcff37abfea6a80d0916a47e8e4d01abb3
Tekin Suleyman <tekin@tekin.co.uk>
2023-12-05 19:01:51 +0000

    Update Game.parse to extract max cube counts
    Considered using named captures and a regular expression that finds all
    the matches at once but I'm playing catchup and actually I hate working
    with regular expressions so let's just plough on.

--- 2023/02/day_two.rb
+++ 2023/02/day_two.rb
@@ -4,15 +4,24 @@ class DayTwo
 
   class Game
     GAME_ID_MATCHER = /^Game (\d+):/
-    attr_reader :id
+    attr_reader :id, :max_red, :max_green, :max_blue
 
     def self.parse(line)
+      rounds = line.split(": ").last.split("; ")
+
       id = GAME_ID_MATCHER.match(line)[1].to_i
-      new(id:)
+      max_red = rounds.map { |round| /(\d+) red/.match(round); $1.to_i }.max
+      max_green = rounds.map { |round| /(\d+) green/.match(round); $1.to_i }.max
+      max_blue = rounds.map { |round| /(\d+) blue/.match(round); $1.to_i }.max
+
+      new(id:, max_red:, max_green:, max_blue:)
     end
 
-    def initialize(id:)
+    def initialize(id:, max_red:, max_green:, max_blue:)
       @id = id
+      @max_red = max_red
+      @max_green = max_green
+      @max_blue = max_blue
     end
   end
 end
--- spec/2023/02/game_spec.rb
+++ spec/2023/02/game_spec.rb
@@ -3,15 +3,24 @@ require "2023/02/day_two"
 RSpec.describe DayTwo::Game do
   describe '.parse' do
 
-    it 'returns a Game instance with the ID extracted from the input' do
+    it 'returns a Game instance with the ID and max cube counts extracted from the input' do
       game = DayTwo::Game.parse("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
       expect(game.id).to eql 5
+      expect(game.max_red).to eql 6
+      expect(game.max_blue).to eql 2
+      expect(game.max_green).to eql 3
 
-      game = DayTwo::Game.parse("Game 35: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+      game = DayTwo::Game.parse("Game 35: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")
       expect(game.id).to eql 35
+      expect(game.max_red).to eql 14
+      expect(game.max_blue).to eql 15
+      expect(game.max_green).to eql 3
 
-      game = DayTwo::Game.parse("Game 835: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+      game = DayTwo::Game.parse("Game 835: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
       expect(game.id).to eql 835
+      expect(game.max_red).to eql 4
+      expect(game.max_blue).to eql 6
+      expect(game.max_green).to eql 2
     end
   end
 end
