ed9883fada604cce0a7c7247fcdf5006c5e0c997
Tekin Suleyman <tekin@tekin.co.uk>
2023-12-06 12:08:45 +0000

    Use StringScanner to capture the numbers' column

--- 2023/03/number_locator.rb
+++ 2023/03/number_locator.rb
@@ -1,13 +1,33 @@
+require "strscan"
+
 class NumberLocator
-  Number = Data.define(:value, :row)
+  NUMBER_MATCH = /(\d+)/
+  Number = Data.define(:value, :row, :col)
 
   def initialize(input)
     @input = input
   end
 
   def numbers
-    rows.each_with_index.map do |row, index|
-      row.scan(/(\d+)/).flatten.map { |string| Number.new(value: string.to_i, row: index) }
+    rows.each_with_index.map do |line, row|
+      scanner = StringScanner.new(line)
+      numbers = []
+
+      until scanner.eos? do
+        break unless scanner.check_until(NUMBER_MATCH)
+
+        value = scanner.matched.to_i
+        # advance to the first digit
+        scanner.scan_until /\d/
+        # use the position to grab the column for the first digit
+        col = scanner.pos - 1
+        # advance to the end of the current number (first non-digit)
+        scanner.skip_until /\D/
+        # Capture our Number object
+        numbers << Number.new(value:, row:, col:)
+      end
+
+      numbers
     end.flatten
   end
 
